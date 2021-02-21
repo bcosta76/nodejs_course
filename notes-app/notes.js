50243a0b255427717017c0bbe9de066bb0ca27f6
Bruno Costa <bcosta76@gmail.com>
2021-02-21 13:06:24 -0100

    Adding save capabilitis to notes app

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,31 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return "Your notes..."
 }
 
 const addNote = function (title, body) {
+    const notes = loadNotes();
+    notes.push({
+        title: title,
+        body: body
+    });
 
+    saveNotes(notes);
+}
+
+const saveNotes = function (notes) {
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
 }
 
 module.exports = {
